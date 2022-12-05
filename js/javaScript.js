33315c28abe2bf255edf915e2cb2d295744696b9
SarnasKadir <92586809+SarnasKadir@users.noreply.github.com>
2022-12-05 09:56:07 +0100

    Update javaScript.js

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,1 +1,13 @@
 
+$(document).ready(function() {
+	$("#showMenu").click(function() {
+		$("#showMenu").hide();
+		$("#hideMenu").show();
+		$(".top_banner").addClass("top_bannerB");
+	});
+	$("#hideMenu").click(function() {
+		$("#hideMenu").hide();
+		$("#showMenu").show();
+		$(".top_banner").removeClass("top_bannerB");
+	});
+});
